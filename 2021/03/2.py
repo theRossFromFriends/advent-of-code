b67909d676b3011866c614d3e720216bbc83baa5
theRossFromFriends <weronika.maniszewska@gmail.com>
2021-12-03 20:05:42 +0100

    Wrap in a function

--- 2021/03/2.py
+++ 2021/03/2.py
@@ -2,42 +2,39 @@ import csv
 import numpy as np
 import copy
 fileObj = open('input', "r")
-lines_oxygen = np.array(fileObj.read().splitlines())
-lines_co2 = copy.deepcopy(lines_oxygen)
+lines = np.array(fileObj.read().splitlines())
+# lines_co2 = copy.deepcopy(lines_oxygen)
 
-oxygen_generator_rating = 0
-for i in range(0, len(lines_oxygen[0])):
-    bit_count = [0, 0]
-    for j in range(0, len(lines_oxygen)):
-        line = lines_oxygen[j]
-        if line[i] == '0':
-            bit_count[0] += 1
+
+def get_rating(input, most_common=True):
+    input_copy = copy.deepcopy(input)
+    for i in range(0, len(input_copy[0])):
+        bit_count = [0, 0]
+        for j in range(0, len(input_copy)):
+            line = input_copy[j]
+            if line[i] == '0':
+                bit_count[0] += 1
+            else:
+                bit_count[1] += 1
+
+        if (bit_count[0] == 0 or bit_count[1] == 0):
+            continue
+
+        if bit_count[0] > bit_count[1]:
+            if most_common:
+                input_copy = [x for x in input_copy if x[i] == '0']
+            else:
+                input_copy = [x for x in input_copy if x[i] == '1']
         else:
-            bit_count[1] += 1
+            if most_common:
+                input_copy = [x for x in input_copy if x[i] == '1']
+            else:
+                input_copy = [x for x in input_copy if x[i] == '0']
 
-    if bit_count[0] > bit_count[1]:
-        lines_oxygen = [x for x in lines_oxygen if x[i] == '0']
-    else:
-        lines_oxygen = [x for x in lines_oxygen if x[i] == '1']
-oxygen_generator_rating = int(lines_oxygen[0], 2)
+    return int(input_copy[0], 2)
 
-co2_scrubber_rating = 0
-for i in range(0, len(lines_co2[0])):
-    bit_count = [0, 0]
-    for j in range(0, len(lines_co2)):
-        line = lines_co2[j]
-        if line[i] == '0':
-            bit_count[0] += 1
-        else:
-            bit_count[1] += 1
 
-    if (bit_count[0] == 0 or bit_count[1] == 0):
-        continue
-
-    if bit_count[0] > bit_count[1]:
-        lines_co2 = [x for x in lines_co2 if x[i] == '1']
-    else:
-        lines_co2 = [x for x in lines_co2 if x[i] == '0']
-co2_scrubber_rating = int(lines_co2[0], 2)
+oxygen_generator_rating = get_rating(lines)
+co2_scrubber_rating = get_rating(lines, most_common=False)
 
 print(oxygen_generator_rating*co2_scrubber_rating)
