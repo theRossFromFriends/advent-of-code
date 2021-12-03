a8239b4cafe0fd066203826cb803b5779646e8d8
nikamnszwsk <w.maniszewska@campus.lmu.de>
2021-12-03 18:42:30 +0100

    Add 3.1

---
+++ 2021/03/1.py
@@ -0,0 +1,27 @@
+import csv
+fileObj = open('input', "r")
+lines = fileObj.read().splitlines()
+line_length = len(lines[0])
+
+bit_count = [[0, 0] for x in range(line_length)]
+for i in range(0, len(lines)):
+    line = lines[i]
+    for j in range(0, line_length):
+        if line[j] == '0':
+            bit_count[j][0] += 1
+        else:
+            bit_count[j][1] += 1
+
+gamma = '0b'
+epsilon = '0b'
+for i in range(0, line_length):
+    if bit_count[i][0] > bit_count[i][1]:
+        gamma += '0'
+        epsilon += '1'
+    else:
+        gamma += '1'
+        epsilon += '0'
+gamma = int(gamma, 2)
+epsilon = int(epsilon, 2)
+
+print(gamma*epsilon)
