d0bdb1030e3711fd2210cae8f5eeea142268b823
Weronika <weronika.maniszewska@gmail.com>
2021-12-08 14:04:27 +0100

    Add 8.1

---
+++ 2021/08/1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+fileObj = open('input', "r")
+input_data = np.array(fileObj.read().splitlines())
+
+outputs = [z.split() for z in [y[1] for y in [x.split('|') for x in input_data]]]
+
+print(len([x for line in outputs for x in line if (len(x) in [2,3,4,7])]))
