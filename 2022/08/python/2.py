6e30c55fd7cc045789cdda4d79a18b06eb389a28
theRossFromFriends <weronika.maniszewska@gmail.com>
2022-12-09 15:24:21 +0100

    Small syntax fix

--- 2022/08/python/2.py
+++ 2022/08/python/2.py
@@ -6,11 +6,11 @@ readData = fileObj.read().split('\n')
 heights = []
 scenic_scores = []
 for line in readData:
-    row = [int(element)
-           for element in list(line)]  # 'x' for not visible
-    scenic_score = [1 for element in list(line)]
-    heights.append(row)
-    scenic_scores.append(scenic_score)
+    heights_row = [int(element)
+                   for element in list(line)]
+    scenic_score_row = [1 for element in list(line)]
+    heights.append(heights_row)
+    scenic_scores.append(scenic_score_row)
 height_array = np.array(heights)
 scenic_scores_array = np.array(scenic_scores)
 
@@ -18,37 +18,29 @@ for (x, y), height in np.ndenumerate(height_array):
     scenic_score = 1
     trees_in_line = 0
     for i in reversed(range(0, x)):
-        if(height_array[i, y] < height):
-            trees_in_line += 1
-        else:
-            trees_in_line += 1
+        trees_in_line += 1
+        if(height_array[i, y] >= height):
             break
     scenic_score *= trees_in_line
 
     trees_in_line = 0
     for i in reversed(range(0, y)):
-        if(height_array[x, i] < height):
-            trees_in_line += 1
-        else:
-            trees_in_line += 1
+        trees_in_line += 1
+        if(height_array[x, i] >= height):
             break
     scenic_score *= trees_in_line
 
     trees_in_line = 0
     for i in range(x+1, len(height_array)):
-        if(height_array[i, y] < height):
-            trees_in_line += 1
-        else:
-            trees_in_line += 1
+        trees_in_line += 1
+        if(height_array[i, y] >= height):
             break
     scenic_score *= trees_in_line
 
     trees_in_line = 0
     for i in range(y+1, len(height_array.T)):
-        if(height_array[x, i] < height):
-            trees_in_line += 1
-        else:
-            trees_in_line += 1
+        trees_in_line += 1
+        if(height_array[x, i] >= height):
             break
     scenic_score *= trees_in_line
 
